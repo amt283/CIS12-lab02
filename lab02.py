e85975eef663c05bca737a4f46643a9c9b7719cb
amt954 <athompson283@gmail.com>
2025-02-01 21:13:25 -0800

    Initial push

---
+++ lab02.py
@@ -0,0 +1,18 @@
+def main():
+    name = "Your Name"
+    age = 20
+    height = 5.7
+    favorite_color = "Blue"
+
+    print("Part 1: Variables and Assignments\n")
+    print("1. Print one variable at a time: \n")
+    print(name)
+    print(age)
+    print(height)
+    print(favorite_color)
+
+    print("\n2. Print with one print statement and commas: \n")
+    print(name, age, height, favorite_color, sep=", ")
+
+
+main()
